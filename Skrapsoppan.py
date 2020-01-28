d29b3616f9369fc0c2ce07b2ae159bfbd39f341e
CallMeShiro <jimkarhulinna@hotmail.com>
2020-01-28 11:33:41 +0100

    Update Skrapsoppan.py

--- Skrapsoppan.py
+++ Skrapsoppan.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup as soup  # HTML data structure
 from urllib.request import urlopen as uReq  # Web client
 
 # opens the connection and downloads html page from url
-uClient = uReq(page_url)
+uClient = uReq("https://www.amazon.com/Cards-Against-Humanity-LLC-CAHUS/dp/B004S8F7QM/ref=sxin_0_osp48-65351ba5_cov?ascsubtag=65351ba5-a53b-4f39-bc09-fd8b9f6bb349&creativeASIN=B004S8F7QM&cv_ct_cx=game&cv_ct_id=amzn1.osp.65351ba5-a53b-4f39-bc09-fd8b9f6bb349&cv_ct_pg=search&cv_ct_wn=osp-search&keywords=game&linkCode=oas&pd_rd_i=B004S8F7QM&pd_rd_r=9f8df896-a81f-4959-855f-771845f6ae24&pd_rd_w=1mMMs&pd_rd_wg=vyH9a&pf_rd_p=62c00474-6fe0-420f-9956-a05256e04b43&pf_rd_r=PBSXE3PCE1G1BX6MZC19&qid=1580207572&sr=1-1-32a32192-7547-4d9b-b4f8-fe31bfe05040&tag=spyonsite-20")
 
 # parses html into a soup data structure to traverse html
 # as if it were a json data type.
